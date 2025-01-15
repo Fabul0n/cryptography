2d8af315401f05a7ce35a3a1498dc3d01a3cbed0
fabulon <fabulon>
2025-01-16 00:41:19 +0700

    ElGamal elyptic done

--- 20/main.py
+++ 20/main.py
@@ -1,39 +1,40 @@
-import sympy
-import random
 import os
-import math
-import numpy as np
+from fastecdsa.curve import Curve
+from fastecdsa.point import Point
+import random
+import sympy
 
+p=115792089210356248762697446949407573530086143415290314195533631308867097853951
+a=-3
+b=int("5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b", 16)
+q=115792089210356248762697446949407573529996955224135760342422259061068512044369
+gx=int("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16)
+gy=int("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16)
 
-def generate_prime(bit_length):
-    return sympy.randprime(2**(bit_length-1), 2**bit_length)
+curve = Curve(
+    name="Some curve",
+    p=115792089210356248762697446949407573530086143415290314195533631308867097853951,
+    a=-3,
+    b=int("5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b", 16),
+    q=115792089210356248762697446949407573529996955224135760342422259061068512044369,
+    gx=int("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16),
+    gy=int("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16)
+)
 
-def extended_gcd(a, b):
-    if a == 0:
-        return b, 0, 1
-    else:
-        g, x, y = extended_gcd(b % a, a)
-        return g, y - (b // a) * x, x
-
-bit_length = 1024
-
-
+G = Point(int("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16), int("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16), curve)
 
 class User:
     def __init__(self, name):
         self.name = name
         self.message = None
-        self.p = 115792089210356248762697446949407573530086143415290314195533631308867097853951
-        self.a = -3
-        self.b = int("5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b", 16)
-        self.n = 115792089210356248762697446949407573529996955224135760342422259061068512044369
-        self.q = self.n
-        self.xG = int("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16)
-        self.yG = int("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16)
+        self.curve = curve
+        self.c = random.randint(1, p-1)
+        self.D = self.c * G
 
 
 
-                            
+    def get_D(self):
+        return self.D
 
     def __repr__(self):
         return self.name + ' ' + str(self.message)
@@ -44,14 +45,18 @@ class User:
     def get_message(self):
         return self.message
 
+    def encrypt_message(self, recipient_D):
+        k = random.randint(1, q-1)
+        R = k * G
+        P = k * recipient_D
+        e = self.message * P.x % p
+        return R, e
 
     
-    def encrypt_message(self, recipient_n, recipient_e, recipient_c, recipients_s):
-        pass
-
-    
-    def decrypt_message(self, message, b1, b2):
-        pass
+    def decrypt_message(self, R, e):
+        Q = self.c * R
+        m = e * sympy.mod_inverse(Q.x, p) % p
+        return m
         
 
 def main():
@@ -80,8 +85,8 @@ def main():
                 user2_id = int(input("Введите номер пользователя - получателя: "))
                 user1 = user_list[user1_id]
                 user2 = user_list[user2_id]
-                (encrypted_mes, b1, b2) = user1.encrypt_message(user2.get_n(), user2.get_e(), user2.get_c(), user2.get_s())
-                print(f"Полученное сообщение: {user2.decrypt_message(encrypted_mes, b1, b2)}")
+                (R, e) = user1.encrypt_message(user2.get_D())
+                print(f"Полученное сообщение: {user2.decrypt_message(R, e)}")
                 input('Нажмите любую клавишу чтобы продолжить')
             case 4:
                 break
@@ -89,6 +94,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-    # 5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b
